19322673122aa055b5139497124f3a871e7ea551
jimmosca <jaime.casado.a@gmail.com>
2019-05-28 09:21:11 +0200

    modificado script

--- script.js
+++ script.js
@@ -4,25 +4,22 @@ function verificarUsuario() {
   objJSON = JSON.stringify(obj);
 
   var xhttp = new XMLHttpRequest();
-  xhttp.open("POST", "http://localhost/mic/GetoBeerIt/comprobarUsr.php");
+  xhttp.open("POST", "http://localhost/jaime/PHP/TiendaCerveza/comprobarUsr.php");
   xhttp.setRequestHeader("Content-Type", "application/json");
 
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      console.log("vamo bien");
 
       var respuestaJSON = JSON.parse(this.responseText);
       var usr = document.getElementById("usr");
       var msjError = document.getElementById("msjError");
 
       if(respuestaJSON["isContained"]){
-        console.log("vamo mejor");
         usr.style.color = "red";
         msjError.className = "d-inline";
         msjError.style.color = "red";
 
       }else{
-        console.log("entro");
         usr.style.color = "black";
         msjError.className = "d-none";
       }
